509fc81050987170379ecba57a6b2194a300b5a1
mridulikamukerji <mridulikamukerji@gmail.com>
2025-03-06 09:30:00 +0530

    Fixed login issues

--- app/src/main/java/com/example/quickbitez/LoginActivity.java
+++ app/src/main/java/com/example/quickbitez/LoginActivity.java
@@ -10,6 +10,7 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText editTextEmail, editTextPassword;
